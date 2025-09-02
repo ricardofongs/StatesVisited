c71bf3d49442d5b86655d8f061351788baf274fb
Ricardo Fong <rfdigitaldev@gmail.com>
2025-09-01 20:58:08 -0400

    Initial Commit

---
+++ StatesVisited/StatesVisitedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatesVisitedApp.swift
+//  StatesVisited
+//
+//  Created by Ricardo Fong on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct StatesVisitedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StatesVisitedTests/StatesVisitedTests.swift
@@ -0,0 +1,17 @@
+//
+//  StatesVisitedTests.swift
+//  StatesVisitedTests
+//
+//  Created by Ricardo Fong on 9/1/25.
+//
+
+import Testing
+@testable import StatesVisited
+
+struct StatesVisitedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
